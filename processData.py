7cacddf8b8858aebd829fa638091145a25e55ad2
Narek Geghamyan <12162743+narek-g@users.noreply.github.com>
2022-01-07 13:13:55 -0800

    init

---
+++ processData.py
@@ -0,0 +1,25 @@
+# proprocess data 
+
+import cv2
+import matplotlib.pyplot as plt
+# import matplotlib.image as mpimg
+import numpy as np
+import pandas as pd
+import json 
+
+import os
+
+
+
+class process(object):
+    
+    def __init__(self, HEIGHT, WIDTH):
+        self.HEIGHT = HEIGHT
+        self.WIDTH = WIDTH
+        self.category_num = 46+1 
+    
+    def make_mask(self, df):
+        pass 
+    
+    def create_image_batch(df): 
+        pass 
