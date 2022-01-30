a6a38fc84d8dd965cddecaed60c6077c8ec4edaf
Narek Geghamyan <12162743+narek-g@users.noreply.github.com>
2022-01-30 08:46:49 -0800

    enable sem. segmentation data loading

--- prototypes/testPytorchMetrics.py
+++ prototypes/testPytorchMetrics.py
@@ -1,14 +1,11 @@
 
 
 
-
-
-
-
-
 class TestPytorchMetrics(object):
     
-    def __init__(self, train_img, seg_img, img_name_list):
-        self.train_img = train_img
-        self.seg_img = seg_img
-        self.img_name_list = img_name_list
+    def __init__(self):
+        pass 
+        
+        
+    def IOU(self):
+        pass 
